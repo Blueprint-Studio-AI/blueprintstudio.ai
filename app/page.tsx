c3eebe5266c7e37bf2ace8efc76638bcd8c47e0f
Jaidon <jaidon.lalor@gmail.com>
2024-09-01 20:41:55 -0600

    build test 2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import GestureTestBox2 from "@/components/tests/GestureTestBox2";
-import GestureHandler from '@/features/GestureHandler';
+import GestureHandler from '../features/GestureHandler';
 import TikTok from '@/components/home/TikTok'
 import { usePreventDefaultScroll } from "@/utils/hooks/usePreventDefaultScroll";
 
