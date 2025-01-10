d0ccfadcedb3628f8ced64d1db45e8e30dab5aac
Evgeniy Kiryanow <kirjeka19977@gmail.com>
2025-01-10 17:04:35 +0200

    start to add footer section

--- components/home/TikTok/content/projects.ts
+++ components/home/TikTok/content/projects.ts
@@ -1,6 +1,7 @@
 import IDScanner from "./IDScanner";
 import ReportGPT from "./ReportGPT";
 import SDN from "./SDN";
+import FooterSection from "./FooterSection";
 
 // This could be generated at build time,
 // but for now we can to manually insert them bc a lot easier
@@ -9,6 +10,7 @@ export const PROJECTS = {
     IDScanner,
     ReportGPT,
     SDN,
+    FooterSection
 } as const;
 
 export const PROJECT_NAMES = Object.keys(PROJECTS);
