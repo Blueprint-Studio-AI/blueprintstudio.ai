6b0ff8cb5c45a5298e840398b6f159b56dc36b1b
Evgeniy Kiryanow <kirjeka19977@gmail.com>
2025-01-10 16:27:23 +0200

    add style for hero page

--- components/home/TikTok/content/HeroSection/index.tsx
+++ components/home/TikTok/content/HeroSection/index.tsx
@@ -1,23 +1,27 @@
 import { ProjectProps } from "../types";
 import Implint from "./images/imprint.svg";
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function HeroSection({ isActive }: ProjectProps) {
-    return (
-        <div className="text-black">
-            <div className="hero-wrapper w-80 my-0 mx-auto">
-            <h1 className="text-center text-5xl font-base">Design <br></br> & Build</h1>
-            <h3 className="font-small text-center pt-6">We partner with founders to shape their future.</h3>
-            <button>
-            <Image
-                priority
-                src={Implint}
-                alt="Follow us on Twitter"
-                />
-                <span>Start</span>
-                <span>Learn</span>
-            </button>
-            </div>
+  return (
+    <div className="text-black pt-[16vh] h-full">
+      <div className="hero-wrapper w-80 my-0 mx-auto h-full flex flex-col justify-around">
+        <div>
+        <h1 className="text-center text-8xl font-base">
+          Design <br></br> & Build
+        </h1>
+        <h3 className="font-small text-2xl text-center pt-6">
+          We partner with founders to shape their future.
+        </h3>
         </div>
-    );
+        <div className="w-full flex justify-center">
+          <button className="text-small bg-[#E1E1E133] border border-[#F4F4F7B2] rounded-[32px] p-[2px] flex items-center space-x-[10px]">
+            <Image priority src={Implint} alt="Follow us on Twitter" className="w-[32px]" />
+            <span className="text-[#000] pr-[10px]">Start</span>
+            <span className="text-[#1D1D1F8C] font-bold pr-[10px]">Learn</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
