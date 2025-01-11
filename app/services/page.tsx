0e189852f6fc5859af38ebf045552230a4f8b1ba
Evgeniy Kiryanow <kirjeka19977@gmail.com>
2025-01-11 15:35:11 +0200

    start to fix bug with build command

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -3,6 +3,12 @@
 
 import { ServicesLayout } from "@/service-pages/components/templates/ServicesLayout";
 
-export default function ServicesPage() {
+// export default function ServicesPage() {
+//   return <ServicesLayout />;
+// }
+
+const ServicePage = () => {
   return <ServicesLayout />;
-}
+};
+
+export default ServicePage;
