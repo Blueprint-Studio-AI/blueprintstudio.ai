55b805e895bf279dc09657985e81ff71df116c07
Evgeniy Kiryanow <kirjeka19977@gmail.com>
2025-01-22 05:20:41 +0200

    update footer

--- components/home/TikTok/content/FooterSection/index.tsx
+++ components/home/TikTok/content/FooterSection/index.tsx
@@ -3,8 +3,8 @@ import { Footer } from "@/service-pages/components/sections/Footer";
 
 export default function FooterSection({ isActive }: ProjectProps) {
     return (
-        <div className="text-black">
-            <Footer />
+        <div className="text-black h-[100%]">
+            <Footer className="h-[100%]" />
         </div>
     );
 }
