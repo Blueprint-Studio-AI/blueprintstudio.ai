2222d6d28d585d349d7cfe824e344f5c04222267
Evgeniy Kiryanow <kirjeka19977@gmail.com>
2025-01-22 17:15:03 +0200

    remove some style

--- components/home/TikTok/content/FooterSection/index.tsx
+++ components/home/TikTok/content/FooterSection/index.tsx
@@ -4,7 +4,7 @@ import { Footer } from "@/service-pages/components/sections/Footer";
 export default function FooterSection({ isActive }: ProjectProps) {
     return (
         <div className="text-black h-[100%]">
-            <Footer className="h-[100%]" />
+            <Footer />
         </div>
     );
 }
