85803f6887654a81a1e4284f65e465fb996db3f9
Picasso <114183062+stupart@users.noreply.github.com>
2025-01-31 10:08:04 -0800

    lil cleaning

--- components/SolutionsGrid.tsx
+++
@@ -1,9 +0,0 @@
-type Solution = {
-    title: string;
-    slug: string;
-    description: string;
-    icon: string; // URL to tech icon
-    benefits: string[];
-    techLogos?: string[]; // Optional associated technology logos
-  };
-  
