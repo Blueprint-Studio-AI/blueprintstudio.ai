976d35cbf74368b6bccc640d51d444165319a4b8
Picasso <114183062+stupart@users.noreply.github.com>
2025-02-23 21:59:50 -0700

    test

--- scripts/get-linear-teams.ts
+++ scripts/get-linear-teams.ts
@@ -1,22 +1,25 @@
 // scripts/get-linear-teams.ts
-import { LinearClient } from "@linear/sdk";
-import dotenv from "dotenv";
 
-dotenv.config();
+//use to find client id for adding a new task form page
 
-async function getTeams() {
-  const linearClient = new LinearClient({
-    apiKey: "x"
-  });
+// import { LinearClient } from "@linear/sdk";
+// import dotenv from "dotenv";
 
-  const { nodes: teams } = await linearClient.teams();
+// dotenv.config();
+
+// async function getTeams() {
+//   const linearClient = new LinearClient({
+//     apiKey: "x"
+//   });
+
+//   const { nodes: teams } = await linearClient.teams();
   
-  console.log('Available Teams:');
-  teams.forEach(team => {
-    console.log(`Name: ${team.name}`);
-    console.log(`ID: ${team.id}`);
-    console.log('---');
-  });
-}
+//   console.log('Available Teams:');
+//   teams.forEach(team => {
+//     console.log(`Name: ${team.name}`);
+//     console.log(`ID: ${team.id}`);
+//     console.log('---');
+//   });
+// }
 
-getTeams().catch(console.error);
+// getTeams().catch(console.error);
