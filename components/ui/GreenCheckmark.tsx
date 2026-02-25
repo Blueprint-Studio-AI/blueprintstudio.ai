489073fb4418d01122581999d3dab6f9481eca1b
Brandon Kong <95515854+brandon-kong@users.noreply.github.com>
2026-02-25 04:24:39 -0600

    style(ui): make GreenCheckmark className optional

--- components/ui/GreenCheckmark.tsx
+++ components/ui/GreenCheckmark.tsx
@@ -1,9 +1,12 @@
+import { cn } from "@/lib/utils";
 import { Check } from "lucide-react";
 
-export default function GreenCheckmark() {
+export default function GreenCheckmark({ className }: {
+  className?: string;
+}) {
   return (
-    <div className="w-5 h-5 rounded-full flex items-center justify-center shrink-0 border-[1.85px] border-[#22D462]">
-      <Check strokeWidth={3} className="w-3 h-3 text-[#22D462]" />
+    <div className={cn(className, "text-[#22D462] w-5 h-5 rounded-full flex items-center justify-center shrink-0 border-[1.85px] border-[#22D462]")}>
+      <Check strokeWidth={3} className="w-3 h-3" />
     </div>
   );
 }
